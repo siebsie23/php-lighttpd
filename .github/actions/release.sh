af587c3057e08e7e1fd28c53beebea20de451378
Sibren <sibrenvansetten@hotmail.nl>
2024-02-07 14:47:20 +0000

    Dont exit if the inspect command fails

--- .github/actions/release.sh
+++ .github/actions/release.sh
@@ -4,15 +4,15 @@ set -eo pipefail
 
 if [[ "${GITHUB_REF}" == refs/heads/main || "${GITHUB_REF}" == refs/tags/* ]]; then
     # Fetch the latest lighttpd version
-    LIGHTTPD_BUILD_VERSION=$(curl -s https://download.lighttpd.net/lighttpd/releases-1.4.x/latest.txt | grep -oP 'lighttpd-\K[0-9]+\.[0-9]+\.[0-9]+' | sort -V | tail -n1)
+    LIGHTTPD_BUILD_VERSION=$(curl -s https://download.lighttpd.net/lighttpd/releases-1.4.x/latest.txt | grep -oP 'lighttpd-\K[0-9]+\.[0-9]+\.[0-9]+' | sort -V | tail -n1) || true
     # Fetch the php version from docker hub
     docker pull php:${PHP_VERSION}-fpm-alpine
-    PHP_BUILD_VERSION=$(docker inspect -f '{{ index .Config.Env }}' php:${PHP_VERSION}-fpm-alpine | grep -oP 'PHP_VERSION=\K[0-9]+\.[0-9]+\.[0-9]+' | sort -V | tail -n1)
+    PHP_BUILD_VERSION=$(docker inspect -f '{{ index .Config.Env }}' php:${PHP_VERSION}-fpm-alpine | grep -oP 'PHP_VERSION=\K[0-9]+\.[0-9]+\.[0-9]+' | sort -V | tail -n1) || true
 
     # Pull the siebsie23/php-lighttpd image to check if the lighttpd and php version already exists
     docker pull siebsie23/php-lighttpd:${PHP_VERSION}-alpine
-    EXISTING_LIGHTTPD_BUILD_VERSION=$(docker inspect -f '{{ index .Config.Labels }}' siebsie23/php-lighttpd:${PHP_VERSION}-alpine | grep -oP 'LIGHTTPD_VERSION=\K[0-9]+\.[0-9]+\.[0-9]+' | sort -V | tail -n1)
-    EXISTING_PHP_BUILD_VERSION=$(docker inspect -f '{{ index .Config.Labels }}' siebsie23/php-lighttpd:${PHP_VERSION}-alpine | grep -oP 'PHP_VERSION=\K[0-9]+\.[0-9]+\.[0-9]+' | sort -V | tail -n1)
+    EXISTING_LIGHTTPD_BUILD_VERSION=$(docker inspect -f '{{ index .Config.Labels }}' siebsie23/php-lighttpd:${PHP_VERSION}-alpine | grep -oP 'LIGHTTPD_VERSION=\K[0-9]+\.[0-9]+\.[0-9]+' | sort -V | tail -n1) || true
+    EXISTING_PHP_BUILD_VERSION=$(docker inspect -f '{{ index .Config.Labels }}' siebsie23/php-lighttpd:${PHP_VERSION}-alpine | grep -oP 'PHP_VERSION=\K[0-9]+\.[0-9]+\.[0-9]+' | sort -V | tail -n1) || true
 
     if [[ "${LIGHTTPD_BUILD_VERSION}" == "${EXISTING_LIGHTTPD_BUILD_VERSION}" && "${PHP_BUILD_VERSION}" == "${EXISTING_PHP_BUILD_VERSION}" ]]; then
         echo "The image siebsie23/php-lighttpd:${PHP_VERSION} with lighttpd ${LIGHTTPD_BUILD_VERSION} and php ${PHP_BUILD_VERSION} already exists. Skipping the build."
